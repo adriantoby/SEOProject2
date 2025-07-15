63f8011e7a482a1d5f1960a276604627357fba59
Adrian Moreno <morenoax7@gmail.com>
2025-07-15 15:58:09 +0000

    added and rendered templates

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/") # home page
+@app.route("/home")
 def home():
-    return "Welcome to our Trade app!"
+    return render_template("home.html")
 
 @app.route("/about") # about page
 def about():
-    return "This is our about page"
+    return render_template("about.html")
 
 @app.route("/trade") # trade page
 def trade():
-    return "This is our trade page"
+    return render_template("trade.html")
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True)

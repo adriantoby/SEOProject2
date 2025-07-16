ef7998b667412824d7360b90a701f812f90a86c7
Adrian Moreno <morenoax7@gmail.com>
2025-07-16 15:29:31 +0000

    added alpha vantage requests

--- backend/logic.py
+++ backend/logic.py
@@ -16,4 +16,21 @@ headers = {
 # print(response.json())
 
 
-# alpha vantage ()
+# alpha vantage (RSI, current_price, SMA)
+alpha_vantage_api_key = os.getenv("ALPHA_VANTAGE_API_KEY")
+alpha_vantage_base_url = "https://www.alphavantage.co/query?function="
+
+# RSI
+RSI_portion = 'RSI&symbol=IBM&interval=weekly&time_period=10&series_type=open&apikey=ALPHA_VANTAGE_API_KEY'
+# response = requests.get(alpha_vantage_base_url + RSI_portion)
+# print(response.json())
+
+# SMA
+SMA_portion = "SMA&symbol=IBM&interval=weekly&time_period=10&series_type=open&apikey=ALPHA_VANTAGE_API_KEY"
+# response = requests.get(alpha_vantage_base_url + SMA_portion)
+# print(response.json())
+
+# current price
+current_price_portion = "GLOBAL_QUOTE&symbol=IBM&apikey=ALPHA_VANTAGE_API_KEY"
+# response = requests.get(alpha_vantage_base_url + current_price_portion)
+# print(response.json())

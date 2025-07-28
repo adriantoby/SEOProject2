fc6873941b73d5f27bfc75d5498a4b760b060be9
Adrian Moreno <morenoax7@gmail.com>
2025-07-28 16:46:35 +0000

    created supabase user signup

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, url_for, flash, redirect, request
 from forms import RegistrationForm
 from flask_behind_proxy import FlaskBehindProxy
+from supabase import create_client, Client
 from backend.logic import ask_gemini
 from backend.decision_logic import process_stock
 from backend.test_db import return_stocks, add_user
@@ -11,6 +12,10 @@ load_dotenv()
 app = Flask(__name__)
 proxied = FlaskBehindProxy(app)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+
+url = os.getenv("SUPABASE_URL")
+key = os.getenv("SUPABASE_KEY")
+supabase = create_client(url, key)
 
 @app.route("/") # home page
 @app.route("/home")
@@ -49,12 +54,18 @@ def assistant():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
-    form = RegistrationForm()
-    if form.validate_on_submit(): # checks if entries are valid
-        flash(f'Account created for {form.username.data}!', 'success')
-        add_user(form.username.data, form.email.data, form.password.data)
-        return redirect(url_for('home')) # if so - send to home page
-    return render_template('register.html', title='Register', form=form)
+    if request.method == "POST":
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        try:
+            user = supabase.auth.sign_up({"email": email, "password": password})
+            print("hi")
+            return redirect(url_for('home'))
+        except Exception as e:
+            print(f"Error during registration: {e}")
+            return f"Error: {e}"
+    return render_template('register.html')
             
 if __name__ == "__main__":
     app.run(debug=True)
